8dff2fd477ed436870597a9effd5ac27a880ef09
victorgoecking <victorgoecking96@gmail.com>
2021-09-17 00:07:47 -0300

    :sparkles: Configurando pagina order_registration

--- resources/views/pages/order/order_registration.blade.php
+++ resources/views/pages/order/order_registration.blade.php
@@ -81,6 +81,204 @@
             </div>
         </div>
 
+        <div class="card shadow mb-4">
+            <div class="card-header">
+                <i class="fas fa-fw fa-cube"></i> Produtos
+            </div>
+            <div class="card-body">
+                <div class="form-row">
+
+
+                    <div class="col-md-12 table-responsive">
+                        <table class="table table-bordered table-hover table-sortable" id="tab_logic">
+                            <thead>
+                            <tr >
+                                <th class="text-center">
+                                    Produto *
+                                </th>
+                                <th class="text-center">
+                                    Quantidade
+                                </th>
+                                <th class="text-center">
+                                    Valor *
+                                </th>
+                                <th class="text-center">
+                                    Desconto
+                                </th>
+                                <th class="text-center">
+                                    Subtotal
+                                </th>
+                                <th class="text-center">
+{{--                                <th class="text-center" style="border-top: 1px solid #ffffff; border-right: 1px solid #ffffff;">--}}
+                                    Del
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr id='addr0' data-id="0" class="hidden">
+                                <td data-name="produto">
+                                    <input type="text" name='name0'  placeholder='Name' class="form-control"/>
+                                </td>
+                                <td data-name="quantidade">
+                                    <input type="text" name='mail0' placeholder='Email' class="form-control"/>
+                                </td>
+                                <td data-name="valor">
+                                    <input name="desc0" placeholder="Description" class="form-control"></input>
+                                </td>
+                                <td data-name="desconto">
+                                    <input type="text" name='mail0' placeholder='Email' class="form-control"/>
+                                </td>
+                                <td data-name="subtotal">
+                                    <input type="text" name='mail0' placeholder='Email' class="form-control"/>
+                                </td>
+                                <td data-name="del">
+                                    <button name="del0" class='btn btn-danger glyphicon glyphicon-remove row-remove'><span aria-hidden="true">×</span></button>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <a id="add_row" class="btn btn-primary float-right">Add Row</a>
+
+
+                </div>
+            </div>
+        </div>
+
+        <div class="card shadow mb-4">
+            <div class="card-header">
+                <i class="fas fa-fw fa-tools"></i> Serviços
+            </div>
+            <div class="card-body">
+                <div class="form-row">
+
+
+                    <div class="col-md-12 table-responsive">
+                        <table class="table table-bordered table-hover table-sortable" id="tab_logic">
+                            <thead>
+                            <tr >
+                                <th class="text-center">
+                                    Produto *
+                                </th>
+                                <th class="text-center">
+                                    Quantidade
+                                </th>
+                                <th class="text-center">
+                                    Valor *
+                                </th>
+                                <th class="text-center">
+                                    Desconto
+                                </th>
+                                <th class="text-center">
+                                    Subtotal
+                                </th>
+                                <th class="text-center">
+                                    {{--                                <th class="text-center" style="border-top: 1px solid #ffffff; border-right: 1px solid #ffffff;">--}}
+                                    Del
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr id='addr0' data-id="0" class="hidden">
+                                <td data-name="produto">
+                                    <input type="text" name='name0'  placeholder='Name' class="form-control"/>
+                                </td>
+                                <td data-name="quantidade">
+                                    <input type="text" name='mail0' placeholder='Email' class="form-control"/>
+                                </td>
+                                <td data-name="valor">
+                                    <input name="desc0" placeholder="Description" class="form-control"></input>
+                                </td>
+                                <td data-name="desconto">
+                                    <input type="text" name='mail0' placeholder='Email' class="form-control"/>
+                                </td>
+                                <td data-name="subtotal">
+                                    <input type="text" name='mail0' placeholder='Email' class="form-control"/>
+                                </td>
+                                <td data-name="del">
+                                    <button name="del0" class='btn btn-danger glyphicon glyphicon-remove row-remove'><span aria-hidden="true">×</span></button>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <a id="add_row" class="btn btn-primary float-right">Add Row</a>
+
+
+                </div>
+            </div>
+        </div>
+
+        <div class="card shadow mb-4">
+            <div class="card-header">
+                <i class="fas fa-map-marker-alt"></i> Endereço de entrega
+            </div>
+            <div class="card-body">
+                <div class="form-row">
+
+
+                    ENDEREÇO
+
+
+                </div>
+            </div>
+        </div>
+
+        <div class="card shadow mb-4">
+            <div class="card-header">
+                <i class="fas fa-truck-moving"></i> Transporte
+            </div>
+            <div class="card-body">
+                <div class="form-row">
+
+
+                    TRANSPORTE
+
+
+                </div>
+            </div>
+        </div>
+
+        <div class="card shadow mb-4">
+            <div class="card-header">
+                <i class="fas fa-dollar-sign"></i> Total
+            </div>
+            <div class="card-body">
+                <div class="form-row">
+
+
+                    TOTAL
+
+
+                </div>
+            </div>
+        </div>
+
+        <div class="card shadow mb-4">
+            <div class="card-header">
+                <i class="fas fa-edit"></i> Observações
+            </div>
+            <div class="card-body">
+                <div class="form-row">
+                    <div class="col-md-6 mb-3">
+                        <i class="fas fa-exclamation-circle"></i> <label class=" font-italic font-weight-bold" for="exampleFormControlNoteClient">Está observação será impressa no pedido.</label>
+                        <textarea class="form-control" name="note_order" id="exampleFormControlNoteOrder" rows="5"></textarea>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <i class="fas fa-exclamation-circle"></i> <label class="font-italic font-weight-bold" for="exampleFormControlNoteClient">Está observação é de uso interno, portanto não será impressa no pedido.</label>
+                        <textarea class="form-control" name="note_order" id="exampleFormControlNoteOrder" rows="5"></textarea>
+                    </div>
+                </div>
+
+                <div class="form-row mt-2">
+                    <button class="btn btn-primary" type="submit"><i class="fas fa-paper-plane"></i> Cadastrar</button>
+                    <a href="{{ route('order.index') }}"><button class="btn btn-danger ml-2" type="button"><i class="fas fa-times-circle"></i> Cancelar</button></a>
+                </div>
+            </div>
+        </div>
+
+
+
     </form>
 
 @endsection
