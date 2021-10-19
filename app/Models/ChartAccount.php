5c81df161417169342d01755b7d0b13b0f2b8486
Victor Goecking <victorgoecking96@gmail.com>
2021-10-19 12:04:47 -0300

    :sparkles: Configurando chart_accounts.blade

--- app/Models/ChartAccount.php
+++ app/Models/ChartAccount.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ChartAccount extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'chart_accounts';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'type',
+        'description',
+    ];
+
+    public function cashMovements(){
+        return $this->hasMany(CashMovement::class, 'chart_account_id', 'id');
+    }
 }
---
+++ resources/views/pages/chart_accounts/chart_accounts.blade.php
@@ -0,0 +1,135 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">
+            <i class="fa fa-users"></i> Usuários&nbsp;
+            <a href="{{ route('user.create') }}">
+                <button type="button" class="btn btn-primary">
+                    <i class="fas fa-plus-circle"></i> Novo
+                </button>
+            </a>
+        </h1>
+
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb m-0 p-2 bg-transparent">
+                <li class="breadcrumb-item"><a href="{{ route('dashboard') }}"><i class="fas fa-fw fa-tachometer-alt"></i> Início</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Usuários</li>
+            </ol>
+        </nav>
+    </div>
+
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped " id="dataTable" width="100%" cellspacing="0">
+{{--                    <thead class="thead-light">--}}
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Nome de Usuário</th>
+                            <th>E-mail</th>
+                            <th>Observação</th>
+                            <th>Nível</th>
+                            <th>Opções</th>
+                        </tr>
+                    </thead>
+
+                    <tfoot>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Nome de Usuário</th>
+                            <th>E-mail</th>
+                            <th>Observação</th>
+                            <th>Nível</th>
+                            <th>Opções</th>
+                        </tr>
+                    </tfoot>
+
+                    <tbody>
+                        @foreach($users as $user)
+                        <tr>
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->username}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>{{$user->note}}</td>
+                            <td>{{$user->level}}</td>
+                            <td class="pt-2">
+{{--                                <a href="{{ route('user.show', ['user' => $user->id]) }}"><button class="btn btn-info btn-sm py-0 px-1 mt-1" data-toggle="modal" data-placement="top" title="Detalhes" data-target="#modalUserDetail"><i class="far fa-eye"></i></button></a>--}}
+                                <a href="{{ route('user.show', ['user' => $user->id]) }}"><button class="btn btn-info btn-sm py-0 px-1 mt-1" data-toggle="tooltip" data-placement="top" title="Detalhes"><i class="far fa-eye"></i></button></a>
+                                <a href="{{ route('user.edit', ['user' => $user->id]) }}"><button class="btn btn-warning btn-sm py-0 px-1 mt-1" data-toggle="tooltip" data-placement="top" title="Editar"><i class="far fa-edit"></i></button></a>
+{{--                                <a href="{{ route('user.show', ['user' => $user->id]) }}"><button class="btn btn-secondary btn-sm py-0 px-1 mt-1" data-toggle="tooltip" data-placement="top" title="Desabilitar"><i class="fas fa-user-slash"></i></button></a>--}}
+                                <form class="d-inline" action="{{ route('user.destroy', ['user' => $user->id]) }}" method="POST">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" onclick="return confirm('Deseja realmente remover o usuário {{ $user->name }} ?')" class="btn btn-danger btn-sm py-0 px-1 mt-1" data-toggle="tooltip" data-placement="top" title="Remover"><i class="far fa-trash-alt"></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    </div>
+
+<!-- Modal User Delete -->
+    <div class="modal fade" id="modalUserDelete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+{{--    @if(isset($_GET['modal']))--}}
+{{--        <!-- Modal User Detail -->--}}
+{{--        <div class="modal fade" id="modalUserDetail" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">--}}
+{{--            <div class="modal-dialog modal-dialog-centered" role="document">--}}
+{{--                <div class="modal-content">--}}
+{{--                    <div class="modal-header">--}}
+{{--                        <h5 class="modal-title" id="exampleModalLongTitle">Detalhes do Usuário</h5>--}}
+{{--                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">--}}
+{{--                            <span aria-hidden="true">&times;</span>--}}
+{{--                        </button>--}}
+{{--                    </div>--}}
+{{--                    <div class="modal-body">--}}
+{{--                        <div class="jumbotron text-center">--}}
+{{--                            <h1 class="display-4">{{ $user->name }}</h1>--}}
+{{--                            <p class="lead"><b>Nome de Usuário:</b> {{ $user->username }}</p>--}}
+{{--                            <p class="lead"><b>E-mail:</b> {{ $user->email }}</p>--}}
+{{--                            <p class="lead"><b>Observação:</b> {{ $user->note }}</p>--}}
+{{--                            <p class="lead"><b>Nivel:</b> {{ $user->level }}</p>--}}
+{{--                            <hr class="my-4">--}}
+{{--                            <p><b>Criado em:</b> {{ $user->created_at }}</p>--}}
+{{--                            <p class="lead">--}}
+{{--                                <a class="btn btn-warning btn-lg" href="{{ route('user.edit', ['user' => $user->id]) }}" role="button"><i class="fas fa-edit"></i> Editar</a>--}}
+{{--                            </p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="modal-footer">--}}
+{{--                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>--}}
+{{--                        <button type="button" class="btn btn-primary">Save changes</button>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    @endif--}}
+@endsection
