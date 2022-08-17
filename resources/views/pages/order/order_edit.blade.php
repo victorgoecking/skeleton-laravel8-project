256a7626cc7860e2ce6cc1a9424e97dc5b31b833
Victor Goecking <victorgoecking96@gmail.com>
2022-08-17 16:21:00 -0300

    :recycle: adjusting orders

--- resources/views/pages/order/order_edit.blade.php
+++ resources/views/pages/order/order_edit.blade.php
@@ -609,121 +609,9 @@
     </script>
 
 
-    {{--<script src="{{ asset('admin/js/order.js')}}"></script>--}}
+    <script src="{{ asset('admin/js/order.js')}}"></script>
+
     <script type="text/javascript">
-
-
-        // ------------------------------------    FORM PEDIDO HIDE CAMPOS
-        $(document).ready(function(){
-            //executar quando a página é carregada
-            esconde();
-
-            //executar todas as vezes que houver alterações do select;
-            $('#selectOrderType').change(function(){
-                esconde();
-            });
-
-
-        });
-
-        function esconde(){
-            //quando tiver editando o formuláro o valor fica no Select
-            selectedValue = $('#selectOrderType').val();
-
-            //quando tiver visualizando o formulário o valor fica no campo text;
-            if(!selectedValue){
-                selectedValue = $('#selectOrderType').text();
-            }
-            switch(selectedValue) {
-                case "0":
-                    $('#divValidity').hide();
-                    $("#divValidity").val("");
-
-                    break;
-                case "1":
-                    $('#divValidity').show();
-                    break;
-                default:
-                    $('#divValidity').hide();
-                    $("#divValidity").val("");
-
-            }
-        }
-
-        //DESABILITA ENVIO POR ENTER
-        $(document).ready(function() {
-            $(window).keydown(function(event){
-                if(event.keyCode == 13) {
-                    event.preventDefault();
-                    return false;
-                }
-            });
-        });
-
-        // SELECTIZE
-        $(document).ready(function () {
-
-            $(".select_selectize").selectize({
-                // create:true, //DAR A OPCAO DE ADICIOANR CASO NAO TIVER
-                sortField: {
-                    field: 'text',
-                    direction: 'asc'
-                },
-                placeholder: $(this).data('placeholder'),
-                // width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
-                allowClear: Boolean($(this).data('allow-clear')),
-
-            });
-
-            $(".select_selectize_client").selectize({
-                // create:true, //DAR A OPCAO DE ADICIOANR CASO NAO TIVER
-                sortField: {
-                    field: 'text',
-                    direction: 'asc'
-                },
-                placeholder: $(this).data('placeholder'),
-                // width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
-                allowClear: Boolean($(this).data('allow-clear')),
-
-                onChange:function (value){
-                    if(value){
-                        document.getElementById('validatyClient').style.display= 'block'
-                        $(".select_selectize_client").removeClass("is-invalid").addClass("is-valid")
-                    }else{
-                        document.getElementById('validatyClient').style.display= 'none'
-                        $(".select_selectize_client").removeClass("is-valid").addClass("is-invalid")
-                    }
-                },
-            });
-
-            $(".select_selectize_address").selectize({
-                // create:true, //DAR A OPCAO DE ADICIOANR CASO NAO TIVER
-                sortField: {
-                    field: 'text',
-                    direction: 'asc'
-                },
-                placeholder: $(this).data('placeholder'),
-                // width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
-                allowClear: Boolean($(this).data('allow-clear')),
-                onFocus: function () {
-                    // control has gained focus
-                    let selectClient = $(document.getElementById('validationCustomClient').value);
-
-                    if(selectClient.prevObject === undefined){
-                        $('#tooltipSearchAddress').tooltip({
-                            'trigger': 'manual',
-                            'title': 'Selecione um cliente por favor.',
-                            'placement': 'top'
-                        }).tooltip('show');
-                    }
-
-                },
-                onBlur: function () {
-                    $('#tooltipSearchAddress').tooltip().tooltip('dispose');
-                },
-
-            });
-        });
 
         function checkClient(){
             document.getElementById('validatyClient').style.display= 'none';
@@ -1071,22 +959,6 @@
                 document.getElementById('containerAddresses').innerHTML = compiled();
             }
         }
-
-        $(document).ready(function() {
-
-            $("#searchProduct").change((event)=> {
-                addProduct(event.target.value);
-            });
-
-            $("#searchService").change((event)=> {
-                addService(event.target.value);
-            });
-
-            $("#searchAddress").change((event)=> {
-                addDeliveryAddress(event.target.value);
-            });
-
-        });
 
         @if($orders_products)
             @foreach($orders_products as $order_product)
